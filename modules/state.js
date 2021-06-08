17dd081cd1b4fef213f5dca14cb63cbfda0cd285
eavichay <eavichay@gmail.com>
2021-06-08 11:46:56 +0300

    some comments

--- modules/state.js
+++ modules/state.js
@@ -3,13 +3,18 @@ import { eventBus } from './messaging.js';
 const tagged = new WeakSet();
 
 export function createState(target = {}, bus = eventBus(), prefix = '') {
+  // primitives cannot have a proxy
   if (typeof target === 'object' && target !== null && !tagged.has(target)) {
     const p = new Proxy(target, {
       get: (t, k) => t[k],
       set: (t, key, value) => {
         const path = prefix ? prefix + '.' + key : key;
         if (t[key] !== value) {
+          // here is the key: If we get an object, we "proxify" it again, recursively
           const newValue = createState(value, bus, path);
+
+          // this weakset ensures an object is proxified once.
+          // the usage of weak references allow the garbage collector to dispose unused objects that are not being watched
           if (tagged.has(value)) {
             t[key] = newValue.state;
           } else {
@@ -24,6 +29,7 @@ export function createState(target = {}, bus = eventBus(), prefix = '') {
         return true;
       },
     });
+    // tag the dataset
     tagged.add(target);
     Object.keys(target).forEach((key) => {
       const path = prefix ? prefix + '.' + key : key;
@@ -35,10 +41,12 @@ export function createState(target = {}, bus = eventBus(), prefix = '') {
       bus.emit(path, upgraded);
       bus.emit('change', { key: path, value: upgraded });
     });
+    // return as state
     return {
       state: p,
       on: bus.on.bind(bus),
     };
   }
+  // return as-is, either already tagged state or primitive
   return target;
 }
